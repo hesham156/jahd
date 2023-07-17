7853e182cad3dab60145fe7f90fdb288da1b656d
hesham156 <h456ad@gmail.com>
2023-07-17 15:31:26 +0200

    CH

--- src/pages/admin/Categores.jsx
+++ src/pages/admin/Categores.jsx
@@ -11,12 +11,12 @@ const [cInstr,setCInstr] = useState();
         <div className='container'>
             <div className='form p-2 rounded-2 bg-white'>
                 <form action="" className='d-flex justify-content-center align-items-center flex-wrap gap-1'> 
-                   <input className='input ' type="text" placeholder='name' name='' onChange={(e)=>{console.log(e.target.value)}}/>
-                   <input className='input ' type="text" placeholder='email' name='' onChange={(e)=>{console.log(e.target.value)}}/>
-                   <input className='input ' type="text" placeholder='pass' name='' onChange={(e)=>{console.log(e.target.value)}}/>
-                   <input className='input ' type="text" placeholder='user' name='' onChange={(e)=>{console.log(e.target.value)}}/>
-                   <input className='input ' type="text" placeholder='ff' name='' onChange={(e)=>{console.log(e.target.value)}}/>
-                   <input className='input ' type="text" placeholder='ff' name='' onChange={(e)=>{console.log(e.target.value)}}/>
+                   <input className='input ' type="text" placeholder='name' name='' onChange={(e)=>{setCName(e.target.value)}}/>
+                   <input className='input ' type="text" placeholder='email' name='' onChange={(e)=>{setCUrl(e.target.value)}}/>
+                   <input className='input ' type="text" placeholder='pass' name='' onChange={(e)=>{setCType(e.target.value)}}/>
+                   <input className='input ' type="text" placeholder='user' name='' onChange={(e)=>{setCInstr(e.target.value)}}/>
+                   {/* <input className='input ' type="text" placeholder='ff' name='' onChange={(e)=>{setC(e.target.value)}}/>
+                   <input className='input ' type="text" placeholder='ff' name='' onChange={(e)=>{setC(e.target.value)}}/> */}
                    <button type='submit'>Send</button>
                 </form>
             </div>
